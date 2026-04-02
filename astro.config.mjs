eb90534a4c88a12c271591299647ecbb255a14d6
JoelArroyoChavez <joel_arroyo_chavez@hotmail.com>
2026-04-01 20:28:49 -0600

    Fix syntax error in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://joel_arroyo_chavez.github.io"
+  site: "https://joel_arroyo_chavez.github.io",
   integrations: [
     tailwind(),
     mdx(),
